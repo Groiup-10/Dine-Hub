1c5af0471f133b08c1073f6ba8aff03ec0c19fde
Scargeo <64209782+Scargeo@users.noreply.github.com>
2024-12-09 21:02:48 +0000

    restarting the development of the backend

--- functions/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { registerUser, verifyToken } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/register', registerUser);
-router.post('/verify-token', verifyToken);
-
-module.exports = router;
